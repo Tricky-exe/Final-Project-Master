bd7787cc32ba4217269f1d7ebd359ebdfe389930
Mikayla <000030993@student.baisd.net>
2022-04-11 14:20:26 -0400

    Create AstronautInfo.java

---
+++ AstronautInfo.java
@@ -0,0 +1,23 @@
+public class AstronautInfo{
+
+private String name;                  //the persons name
+private String dob;                   //the persons date of birth (dob)
+private String serialNumber;          //the persons sequentially generated serial number    // ask dockett if they input their number or if it needs to be randomly generated (if generated, whats the criteria)
+private String address;               //the persons address on Earth 
+private String email;                 //the persons email address
+private String phoneNum;              //the persons phone number
+private String payRate;               //the persons pay rate
+private String weight;                //the persons weight
+private String nextChild;             //the persons next of kin
+private String status;                //the persons status of whether or not they are on Earth
+
+
+
+
+//I have no clue what im doing <3
+
+
+
+
+
+}
