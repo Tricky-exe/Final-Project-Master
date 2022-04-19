79c572caa8398b9631c7a07aff1397465a57dfd9
Mikayla <000030993@student.baisd.net>
2022-04-19 14:24:41 -0400

    Yup
    sorta adding things

--- AstronautInfo.java
+++ AstronautInfo.java
@@ -1,20 +1,31 @@
 public class AstronautInfo{
 
-private String name;                  //the persons name
-private String dob;                   //the persons date of birth (dob)
-private String serialNumber;          //the persons sequentially generated serial number    // ask dockett if they input their number or if it needs to be randomly generated (if generated, whats the criteria)
-private String address;               //the persons address on Earth 
-private String email;                 //the persons email address
-private String phoneNum;              //the persons phone number
-private String payRate;               //the persons pay rate
-private String weight;                //the persons weight
-private String nextChild;             //the persons next of kin
-private String status;                //the persons status of whether or not they are on Earth
+String name;                  //the persons name
+String dob;                   //the persons date of birth (dob)
+String serialNumber;          //the persons sequentially generated serial number    // ask dockett if they input their number or if it needs to be randomly generated (if generated, whats the criteria)
+String address;               //the persons address on Earth 
+String email;                 //the persons email address
+String phoneNum;              //the persons phone number
+int payRate;                  //the persons pay rate
+String weight;                //the persons weight
+String nextChild;             //the persons next of kin
+String status;                //the persons status of whether or not they are on Earth
+
+public AstronautInfo(){
 
 
+}
 
 
-//I have no clue what im doing <3
+public AstronautInfo(String name1, String dob1, String address1){
+    name = name1;
+    dob = dob1;
+    address = address1; 
+
+}
+
+//previous code that ive got open rn    personalinformation, personBuilder, Person, and the Main for that last one
+
 
 
 
