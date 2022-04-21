5c32dcca4b74b0101ba948967ea16f8b50c0ac3c
Mikayla <000030993@student.baisd.net>
2022-04-21 14:37:28 -0400

    Astronaut a c k
    Actually making progress pog

---
+++ AstroInfo.java
@@ -0,0 +1,34 @@
+public class AstroInfo{
+
+String name;                  // The persons name    X
+String dob;                   // The persons date of birth (dob)  X
+String serialNumber;          // The persons sequentially generated serial number    // ask dockett if they input their number or if it needs to be randomly generated (if generated, whats the criteria)
+String address;               // The persons address on Earth   X
+String email;                 // The persons email address
+String phoneNum;              // The persons phone number    X
+int payRate;                  // The persons pay rate
+String weight;                // The persons weight
+String nextChild;             // The persons next of kin
+String status;                // The persons status of whether or not they are on Earth
+
+public AstroInfo(){
+
+
+}
+
+// Accepts string for name, date of birth, and address
+public AstroInfo(String name1, String dob1, String address1){
+    name = name1;
+    dob = dob1;
+    address = address1; 
+
+}
+
+//previous code that ive got open rn    personalinformation, personBuilder   Theyre really helping so keep using them 
+
+
+
+
+
+
+} 
--- AstronautInfo.java
+++ AstronautInfo.java
@@ -1,30 +1,95 @@
-public class AstronautInfo{
+public class AstronautInfo {
+    //this class is to get the information of the astronauts
+    private String name;       // A person's name
+    private int dob;           // A person's date of birth (dob)
+    private String address;    // A person's address
+    private String phone;      // A person's phone number
 
-String name;                  //the persons name
-String dob;                   //the persons date of birth (dob)
-String serialNumber;          //the persons sequentially generated serial number    // ask dockett if they input their number or if it needs to be randomly generated (if generated, whats the criteria)
-String address;               //the persons address on Earth 
-String email;                 //the persons email address
-String phoneNum;              //the persons phone number
-int payRate;                  //the persons pay rate
-String weight;                //the persons weight
-String nextChild;             //the persons next of kin
-String status;                //the persons status of whether or not they are on Earth
+    /**
+     The setName method sets the person's name
+     @param name2 The person's name
+     */
+    public void setName(String name2){
+        name = name2;
+    }
 
-public AstronautInfo(){
+    /**
+     The setAge method sets the person's date of birth
+     @param dob2 The person's age
+     */
+    public void setAge(int dob2){
+        dob = dob2;
+    }
+
+    /**
+     The setAddress method sets the person's address on Earth
+     @param address2 The person's address
+     */
+    public void setAddress(String address2){
+        address = address2;
+    }
+
+    /**
+     The setPhone method sets the person's phone number
+     @param phone2 The person's phone number
+     */
+    public void setPhone(String phone2){
+        phone = phone2;
+    }
 
 
-}
 
 
-public AstronautInfo(String name1, String dob1, String address1){
-    name = name1;
-    dob = dob1;
-    address = address1; 
 
-}
 
-//previous code that ive got open rn    personalinformation, personBuilder, Person, and the Main for that last one
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
 
 
 
